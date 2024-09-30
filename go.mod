18bd6c66cb5daab8a364a34b4b58dfd43e4b2529
Siddhant <s1d.official011001011@gmail.com>
2024-09-30 23:55:56 +0530

    feat: added upload handler, interlace function and some input with result images

---
+++ go.mod
@@ -0,0 +1,9 @@
+module lenticular
+
+go 1.22.3
+
+require (
+	github.com/disintegration/imaging v1.6.2 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	golang.org/x/image v0.20.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"image"
+	"image/png"
+	"net/http"
+	"strconv"
+
+	"github.com/disintegration/imaging"
+	"github.com/gorilla/mux"
+)
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+func interlaceImages(images []image.Image, stripWidth int) (image.Image, error) {
+	width := images[0].Bounds().Dx()
+	height := images[0].Bounds().Dy()
+
+	interlaced := image.NewRGBA(image.Rect(0, 0, width, height))
+	for x := 0; x < width; x++ {
+		imgIndex := (x / stripWidth) % len(images)
+		for y := 0; y < height; y++ {
+			interlaced.Set(x, y, images[imgIndex].At(x, y))
+		}
+	}
+
+	return interlaced, nil
+}
+
+func writeJSONError(w http.ResponseWriter, message string, status int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(ErrorResponse{Error: message})
+}
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(32 << 20)
+	if err != nil {
+		writeJSONError(w, "Unable to parse form", http.StatusBadRequest)
+		return
+	}
+
+	stripWidthStr := r.FormValue("stripWidth")
+	stripWidth, err := strconv.Atoi(stripWidthStr)
+	if err != nil || stripWidth <= 0 {
+		stripWidth = 8
+	}
+
+	files := r.MultipartForm.File["images"]
+	var loadedImages []image.Image
+
+	for _, fileHeader := range files {
+		file, err := fileHeader.Open()
+		if err != nil {
+			writeJSONError(w, "Unable to open uploaded file", http.StatusInternalServerError)
+			return
+		}
+		defer file.Close()
+
+		img, err := imaging.Decode(file)
+		if err != nil {
+			writeJSONError(w, "Failed to decode image", http.StatusBadRequest)
+			return
+		}
+		loadedImages = append(loadedImages, img)
+	}
+
+	interlacedImg, err := interlaceImages(loadedImages, stripWidth)
+	if err != nil {
+		writeJSONError(w, "Failed to interlace images", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "image/png")
+	if err := png.Encode(w, interlacedImg); err != nil {
+		writeJSONError(w, "Failed to encode interlaced image", http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/upload", uploadHandler).Methods("POST")
+
+	fmt.Println("Server started at :8080")
+	http.ListenAndServe(":8080", router)
+}
